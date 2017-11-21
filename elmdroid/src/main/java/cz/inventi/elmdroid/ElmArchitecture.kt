2d1086aeb1abbc621eaf85e617a4d15f807d86be
Tomas Valenta <semanticer@gmail.com>
2017-11-21 18:53:48 +0100

    First Cmd partial implementation

--- elmdroid/src/main/java/cz/inventi/elmdroid/ElmArchitecture.kt
+++ elmdroid/src/main/java/cz/inventi/elmdroid/ElmArchitecture.kt
@@ -1,6 +1,7 @@
 package cz.inventi.elmdroid
 
 import android.arch.lifecycle.LiveData
+import io.reactivex.Single
 
 /**
  * Created by tomas.valenta on 11/16/2017.
@@ -11,10 +12,12 @@ interface ComponentController<STATE : State, in MSG : Msg> {
     fun dispatch(msg: MSG)
 }
 
-interface Component<STATE : State, in MSG : Msg> {
+interface Component<STATE : State, MSG : Msg, CMD : Cmd> {
     fun initState(): STATE
-    fun update(msg: MSG, prevState: STATE): STATE
+    fun update(msg: MSG, prevState: STATE): Pair<STATE, CMD>
+    fun call(cmd: CMD): Single<MSG>
 }
 
 interface State
 interface Msg
+interface Cmd
--- sample/src/main/java/com/example/elmdroid/login/LoginComponent.kt
+++ sample/src/main/java/com/example/elmdroid/login/LoginComponent.kt
@@ -1,28 +1,43 @@
 package com.example.elmdroid.login
 
-import cz.inventi.elmdroid.Component
-import cz.inventi.elmdroid.Msg
-import cz.inventi.elmdroid.State
+import cz.inventi.elmdroid.*
+import io.reactivex.Single
 
 /**
  * Created by tomas.valenta on 11/21/2017.
  */
-class LoginComponent : Component<LoginState, LoginMsg> {
+class LoginComponent : Component<LoginState, LoginMsg, LoginCmd> {
+
+
     override fun initState(): LoginState = LoginState("", "", false,false, "Anon")
 
-    override fun update(msg: LoginMsg, prevState: LoginState): LoginState = when(msg) {
-        is EmailChanged -> prevState.copy(email = msg.email).updateLoginEnabled()
-        is PasswordChanged -> prevState.copy(password = msg.password).updateLoginEnabled()
-        is LoginClicked -> prevState.copy(loadingVisible = true, loginEnabled = false)
+    override fun update(msg: LoginMsg, ps: LoginState): Pair<LoginState, LoginCmd> = when(msg) {
+        is EmailChanged -> ps.copy(email = msg.email).updateLoginEnabled() to None
+        is PasswordChanged -> ps.copy(password = msg.password).updateLoginEnabled() to None
+        is LoginClicked -> ps.copy(loadingVisible = true, loginEnabled = false) to LoginAction(ps.email, ps.password)
+        is Idle -> ps to None
+    }
+
+    override fun call(cmd: LoginCmd): Single<LoginMsg> = when(cmd) {
+        is None -> Single.just(Idle)
+        is LoginAction -> Single.just(Idle) // todo change
     }
 
     private fun LoginState.updateLoginEnabled() = this.copy(loginEnabled = (email.isNotBlank() && password.isNotBlank()))
 
 }
 
+// State
 data class LoginState(val email: String, val password: String, val loginEnabled: Boolean, val loadingVisible: Boolean, val loggedUsername: String) : State
 
+// Msg
 sealed class LoginMsg : Msg
 data class EmailChanged(val email: String): LoginMsg()
 data class PasswordChanged(val password: String): LoginMsg()
 object LoginClicked: LoginMsg()
+object Idle : LoginMsg()
+
+// Cmd
+sealed class LoginCmd : Cmd
+object None : LoginCmd()
+data class LoginAction(val email: String, val password: String) : LoginCmd()
