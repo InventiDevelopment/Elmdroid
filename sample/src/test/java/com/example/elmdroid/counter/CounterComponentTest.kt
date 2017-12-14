2939a468c8b04aff3a548b084440babf02bd0ae5
Tomas Valenta <semanticer@gmail.com>
2017-12-14 16:25:36 +0100

    Add first unit test for counter sample

---
+++ sample/src/test/java/com/example/elmdroid/counter/CounterComponentTest.kt
@@ -0,0 +1,54 @@
+package com.example.elmdroid.counter
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.lifecycle.Observer
+import com.example.elmdroid.utils.RxImmediateSchedulerRule
+import com.example.elmdroid.utils.verifyStates
+import cz.inventi.elmdroid.ElmRuntime
+import org.junit.Before
+import org.junit.ClassRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+
+@RunWith(JUnit4::class)
+class CounterComponentTest {
+
+    @JvmField
+    @Rule
+    @get:Rule val instantExecutorRule = InstantTaskExecutorRule()
+
+    companion object {
+        @JvmField
+        @ClassRule
+        val schedulers = RxImmediateSchedulerRule()
+    }
+
+    private lateinit var component: CounterComponent
+    private lateinit var runtime: ElmRuntime<CounterState, CounterMsg, Nothing>
+    @Mock lateinit var observer: Observer<CounterState>
+
+    @Before
+    fun setup(){
+        MockitoAnnotations.initMocks(this)
+        component = CounterComponent()
+        runtime = ElmRuntime(component)
+    }
+
+    @Test
+    fun simpleUpdate() {
+        verifyStates(runtime, observer,
+                CounterState(0),
+                Increment to CounterState(1),
+                        Decrement to CounterState(0),
+                        Decrement to CounterState(-1),
+                        Decrement to CounterState(-2),
+                        Increment to CounterState(-1),
+                        Increment to CounterState(0)
+                )
+    }
+}
---
+++ sample/src/test/java/com/example/elmdroid/utils/RxImmediateSchedulerRule.java
@@ -0,0 +1,55 @@
+package com.example.elmdroid.utils;
+
+import android.support.annotation.NonNull;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Scheduler;
+import io.reactivex.android.plugins.RxAndroidPlugins;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.internal.schedulers.ExecutorScheduler;
+import io.reactivex.plugins.RxJavaPlugins;
+
+/**
+ * Created by tomas.valenta on 11/15/2017.
+ */
+
+public class RxImmediateSchedulerRule implements TestRule {
+    private Scheduler immediate = new Scheduler() {
+        @Override
+        public Disposable scheduleDirect(@NonNull Runnable run, long delay, @NonNull TimeUnit unit) {
+            // this prevents StackOverflowErrors when scheduling with a delay
+            return super.scheduleDirect(run, 0, unit);
+        }
+
+        @Override
+        public Worker createWorker() {
+            return new ExecutorScheduler.ExecutorWorker(Runnable::run);
+        }
+    };
+
+    @Override
+    public Statement apply(final Statement base, Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                RxJavaPlugins.setInitIoSchedulerHandler(scheduler -> immediate);
+                RxJavaPlugins.setInitComputationSchedulerHandler(scheduler -> immediate);
+                RxJavaPlugins.setInitNewThreadSchedulerHandler(scheduler -> immediate);
+                RxJavaPlugins.setInitSingleSchedulerHandler(scheduler -> immediate);
+                RxAndroidPlugins.setInitMainThreadSchedulerHandler(scheduler -> immediate);
+
+                try {
+                    base.evaluate();
+                } finally {
+                    RxJavaPlugins.reset();
+                    RxAndroidPlugins.reset();
+                }
+            }
+        };
+    }
+}
