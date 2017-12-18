f79e5abae804138d5524bd2f68536c33ae10b5fe
Tomas Valenta <semanticer@gmail.com>
2017-12-18 13:55:30 +0100

    Add initState test for ElmRuntime

---
+++ elmdroid/src/test/java/cz/inventi/elmdroid/ElmRuntimeTest.kt
@@ -0,0 +1,59 @@
+package cz.inventi.elmdroid
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.lifecycle.Observer
+import cz.inventi.elmdroid.utils.RxImmediateSchedulerRule
+import org.junit.Before
+import org.junit.ClassRule
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
+
+
+class ElmRuntimeTest {
+
+    @JvmField
+    @Rule
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var runtime: ElmRuntime<TestState, TestMsg, TestCmd>
+    @Mock lateinit var observer: Observer<TestState>
+    @Mock lateinit var component: Component<TestState, TestMsg, TestCmd>
+
+
+
+    companion object {
+        @JvmField
+        @ClassRule
+        val schedulers = RxImmediateSchedulerRule()
+    }
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+
+    }
+
+    @Test
+    fun initState() {
+        `when`(component.initState()).thenReturn(TestState(3, "init"))
+        runtime = ElmRuntime(component)
+        runtime.state().observeForever(observer)
+        verify(observer).onChanged(TestState(3, "init"))
+    }
+
+}
+
+data class TestState(val number: Int, val text: String): State
+
+sealed class TestMsg : Msg
+object ObjMsg : TestMsg()
+data class NumberMsg(val number: Int) : TestMsg()
+
+sealed class TestCmd: Cmd
+data class NumberCmd(val num: Int): TestCmd()
+
