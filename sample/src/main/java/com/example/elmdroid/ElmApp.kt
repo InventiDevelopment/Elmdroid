6f3346127774def8af5af001ac851b696d87508d
Tomas Valenta <semanticer@gmail.com>
2018-02-13 16:40:35 +0100

    Minor refactoring

--- sample/src/main/java/com/example/elmdroid/ElmApp.kt
+++ sample/src/main/java/com/example/elmdroid/ElmApp.kt
@@ -1,14 +1,10 @@
 package com.example.elmdroid
 
 import android.app.Application
-import timber.log.Timber.DebugTree
 import timber.log.Timber
+import timber.log.Timber.DebugTree
 
 
-
-/**
- * Created by tomas.valenta on 11/21/2017.
- */
 class ElmApp : Application() {
     override fun onCreate() {
         super.onCreate()
